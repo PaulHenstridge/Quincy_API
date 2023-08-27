8aa004a6bab79e7d383e577adc14f2b46fbd92a5
Paul Henstridge <paulhenstridge@gmail.com>
2023-08-27 11:07:42 +0100

    adding routes search by content tag

--- root/static/index.js
+++ root/static/index.js
@@ -1,16 +1,18 @@
 const getAllBtn = document.querySelector('#get-all-btn')
-const searchDescripBtn = document.querySelector('#search-descrip-btn')
 const getRandomBtn = document.querySelector('#get-random-btn')
+const searchDescripBtn = document.querySelector('#search-descrip-btn')
+const searchTagsBtn = document.querySelector('#search-tags-btn')
+const searchTagsPartialBtn = document.querySelector('#search-tags-partial-btn')
 
 getAllBtn.addEventListener('click', () => {
     fetch('/links')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        console.log(data)
         // display data somewhere
       })
       .catch(error => {
-        console.error("Error fetching all links:", error);
+        console.error("Error fetching all links:", error)
       });
 })
 
@@ -19,19 +21,51 @@ getRandomBtn.addEventListener('click', () => {
     fetch('/links/random')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        console.log(data)
         // display data somewhere
       })
       .catch(error => {
-        console.error("Error fetching all links:", error);
+        console.error("Error fetching all links:", error)
       });
 })
 
 
 searchDescripBtn.addEventListener('click', () => {
-    const searchTerm = document.querySelector('#search-input').value;
-    const searchUrl = `/links/search?term=${encodeURIComponent(searchTerm)}`;
+    const searchTerm = document.querySelector('#search-descrip-input').value
+    const searchUrl = `/links/search?term=${encodeURIComponent(searchTerm)}`
 
+    fetch(searchUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // display data somewhere
+      })
+      .catch(error => {
+        console.error("Error fetching search results:", error)
+      });
+})
+
+
+searchTagsBtn.addEventListener('click', () => {
+    const searchTerm = document.querySelector('#search-tags-input').value;
+    const searchUrl = `/links/search_by_tag?tag=${encodeURIComponent(searchTerm)}`;
+    console.log(searchUrl)
+    fetch(searchUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // display data somewhere
+      })
+      .catch(error => {
+        console.error("Error fetching search results:", error);
+      });
+})
+
+
+searchTagsPartialBtn.addEventListener('click', () => {
+    const searchTerm = document.querySelector('#search-tags-partial-input').value;
+    const searchUrl = `/links/search_by_tag_partial?term=${encodeURIComponent(searchTerm)}`;
+    console.log(searchUrl)
     fetch(searchUrl)
       .then(response => response.json())
       .then(data => {
